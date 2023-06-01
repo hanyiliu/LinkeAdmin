2c810aecb86def2cf178c64ef247eacfd774579b
Hanyi Liu <hanyiliu0@gmail.com>
2023-06-01 12:52:32 -0700

    Initial Commit

---
+++ LinkeAdmin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LinkeAdmin
+//
+//  Created by Hanyi Liu on 6/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LinkeAdmin/LinkeAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkeAdminApp.swift
+//  LinkeAdmin
+//
+//  Created by Hanyi Liu on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct LinkeAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
